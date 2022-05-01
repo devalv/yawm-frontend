abb8cf7122ab7bc7496feca9ea322da36fc85cf1
Aleksei <aleksei@devyatkin.dev>
2022-05-01 22:54:11 +0300

    sentry initial configuratoin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+const { REACT_APP_SENTRY_DSN } = process.env;
+
+Sentry.init({
+    debug: true,
+    dsn: REACT_APP_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
