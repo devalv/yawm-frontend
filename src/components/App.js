74737178995a649a30ae1f5403796bd05d0fa980
Aleksei <aleksei@devyatkin.dev>
2022-05-01 22:18:10 +0300

    simple local auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import NewAuthentication from "./auth/authentication";
-import { anonymousUser, AuthContext } from "./auth/auth-context";
+import {anonymousUser, AuthContext, tokenInfo, TokenContext} from "./auth/auth-context";
 import HealthCheck from "./health-check";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavbarC from "./navbar";
@@ -9,27 +9,30 @@ import DetailWishlistCard from "./wishlist/detailWishlistCard";
 
 function App() {
   const [AuthState, setAuthState] = useState(anonymousUser);
+  const [TokenState, setTokenState] = useState(tokenInfo);
 
   return (
     <Router>
-      <AuthContext.Provider value={[AuthState, setAuthState]}>
-        <HealthCheck />
-        <NewAuthentication />
-        <NavbarC
-          authenticated={AuthState.authenticated}
-          username={AuthState.username}
-        />
-        <Switch>
-          <Route exact path="/">
-            <Wishlist />
-          </Route>
-          <Route
-            path="/wishlist/:id"
-            children={<DetailWishlistCard userId={AuthState.user_id} />}
+      <TokenContext.Provider value ={[TokenState, setTokenState]}>
+        <AuthContext.Provider value={[AuthState, setAuthState]}>
+          <HealthCheck />
+          <NewAuthentication />
+          <NavbarC
+            authenticated={AuthState.authenticated}
+            username={AuthState.username}
           />
-          <Route path="/:page" children={<Wishlist />} />
-        </Switch>
-      </AuthContext.Provider>
+          <Switch>
+            <Route exact path="/">
+              <Wishlist />
+            </Route>
+            <Route
+              path="/wishlist/:id"
+              children={<DetailWishlistCard userId={AuthState.user_id} />}
+            />
+            <Route path="/:page" children={<Wishlist />} />
+          </Switch>
+        </AuthContext.Provider>
+      </TokenContext.Provider>
     </Router>
   );
 }
