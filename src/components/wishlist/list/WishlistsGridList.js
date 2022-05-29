5766fb5006ab4bf00ab6f46fcdc46d65c0ec247e
Aleksei <aleksei@devyatkin.dev>
2022-05-29 15:48:04 +0300

    release/react-0.3.0: pagination
    removed old code for list and pagination
    no more healthcheck
    pagination

--- src/components/wishlist/list/WishlistsGridList.js
+++ src/components/wishlist/list/WishlistsGridList.js
@@ -2,72 +2,49 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 
+import Stack from '@mui/material/Stack';
+import Pagination from '@mui/material/Pagination';
 import WishlistItem from "./WishlistGridItem";
 
 
 export default function NestedWishlistsGrid() {
   const { REACT_APP_API_V1_URL } = process.env;
-  const { page } = useParams();
-
-  const [wishlists, setWishlist] = React.useState([]);
-  const [paginationInfo, setPaginationInfo] = React.useState({
-    next: null,
-    prev: null,
-  });
-
-  const extractPaginationPages = (paginationLinks) => {
-    setPaginationInfo({
-      next: null,
-      prev: null,
-      show_prev: null,
-      show_next: null,
-    });
-    const IsShowPrev = paginationLinks.first !== paginationLinks.self;
-    const IsShowNext = paginationLinks.last !== paginationLinks.self;
-    const PrevLink = paginationLinks.prev
-      ? paginationLinks.prev.match(/(?:page=)(\d)/)[1]
-      : null;
-    const NextLink = paginationLinks.next
-      ? paginationLinks.next.match(/(?:page=)(\d)/)[1]
-      : null;
-    setPaginationInfo({
-      next: NextLink,
-      prev: PrevLink,
-      IsShowPrev,
-      IsShowNext,
-    });
+  // Pagination
+  const [selectedPage, setSelectedPage] = React.useState(1);
+  const [pagesCount, setPagesCount] =  React.useState(1);
+  const handlePageChange = (event, value) => {
+    setSelectedPage(value);
   };
-
+  // Wishlists
+  const [wishlists, setWishlist] = React.useState([]);
   React.useEffect(() => {
     const getWishlists = async () => {
-      const producerWishlistEndpoint = page
-        ? `${REACT_APP_API_V1_URL}/wishlist?size=11&page=${page}`
-        : `${REACT_APP_API_V1_URL}/wishlist?size=11`;
-
+      const producerWishlistEndpoint = `${REACT_APP_API_V1_URL}/wishlist?size=12&page=${selectedPage}`;
       await axios.get(producerWishlistEndpoint).then((response) => {
         setWishlist(response.data.items);
-        extractPaginationPages(response.data.links);
-        // TODO: @devalv show pagination buttons
-        console.debug(paginationInfo);
+        setPagesCount(Math.ceil(response.data.total / response.data.size));
       });
     };
-
     getWishlists();
-  }, [page]);
-
+  }, [selectedPage]);
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={1}>
-        <Grid container item spacing={{ xs: 2, md: 4, lg: 6 }} columns={{ xs: 4, sm: 8, md: 12, lg: 20}}>
-          {wishlists.map((wishlist) => (
-            <Grid item key={wishlist.id}>
-                <WishlistItem props={wishlist}/>
-            </Grid>
-          ))}
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid container item spacing={{ xs: 2, md: 4, lg: 6 }} columns={{ xs: 4, sm: 8, md: 12, lg: 20}}>
+            {wishlists.map((wishlist) => (
+              <Grid item key={wishlist.id}>
+                  <WishlistItem props={wishlist}/>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+      <br/>
+      <Stack spacing={2}>
+        <Pagination count={pagesCount} page={selectedPage || 1} onChange={handlePageChange} hideNextButton hidePrevButton color="primary"/>
+    </Stack>
+    </>
   );
 }
