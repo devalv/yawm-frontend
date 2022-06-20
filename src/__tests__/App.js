799bad69c3a8db3ee21a1f1d9a8226dfa441231c
Aleksey Devyatkin <aleksei@devyatkin.dev>
2022-06-20 17:03:30 +0300

    release/react-0.3.0: First test

---
+++ src/__tests__/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+it('check navbar is in place', () => {
+  render(<App />);
+  expect(screen.getByText("Мастер списков")).toBeInTheDocument();
+}, 5);
